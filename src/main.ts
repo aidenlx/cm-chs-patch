75c5e66a712975198ebc8cae7836e0f7e63edd04
AidenLx <aiden.lx@outlook.com>
2021-04-06 19:21:07 +0800

    Update patch method

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,71 @@
 import { Plugin } from "obsidian";
-import { Segment, useDefault } from 'segmentit';
-import findWordAt from 'inject'
+import { Segment, useDefault } from "segmentit";
 
+import type * as CodeMirror from "codemirror";
+import { Pos, Range } from "./cm-extracted";
 
 export default class CMChsPatch extends Plugin {
 
-	segmentit:any;
+  segmentit: any;
+
+  findWordAt_backup? : findWordAt;
 
   async onload() {
     console.log("loading cm-chs-patch");
 
-		this.segmentit = useDefault(new Segment())
+    this.segmentit = useDefault(new Segment());
 
     this.registerCodeMirror((cm: CodeMirror.Editor) => {
-    	cm.findWordAt = findWordAt;
+      patch(cm, this.segmentit);
     });
   }
 
   onunload() {
     console.log("unloading cm-chs-patch");
   }
+}
+
+type findWordAt = (pos: CodeMirror.Position) => CodeMirror.Range;
+
+function patch(cm: CodeMirror.Editor, segmentit: any): void {
+  const src = cm.findWordAt.bind(cm) as findWordAt;
+  function findWordAtPatch(pos: CodeMirror.Position): CodeMirror.Range {
+    // Used to control trim range
+    const range = 6;
+
+    const result = src(pos);
+
+    let line = cm.getLine(pos.line);
+    let start = result.from().ch;
+    let end = result.to().ch;
+
+    // Pass result out directly if no chs present
+    if (!/[\u4e00-\u9fa5]/.test(line.substring(start, end))) {
+      return result;
+    } else {
+      // trim long text
+      if (pos.ch - start > range) {
+        start = pos.ch - range;
+      }
+      if (end - pos.ch > range) {
+        end = pos.ch + range;
+      }
+      let found = line.substring(start, end);
+      let relativePos = pos.ch - start;
+      const result = segmentit.doSegment(found);
+      let chunkStart = 0,
+        chunkEnd;
+      for (const seg of result) {
+        chunkEnd = chunkStart + seg.w.length;
+        if (relativePos >= chunkStart && relativePos < chunkEnd) {
+          break;
+        }
+        chunkStart += seg.w.length;
+      }
+      end = chunkEnd + start;
+      start = chunkStart + start;
+      return new Range(Pos(pos.line, start), Pos(pos.line, end));
+    }
+  }
+  cm.findWordAt = findWordAtPatch;
 }
