62ccc0f736dc1b14e530d586c94bf630aadc8a15
aidenlx <aiden.lx@outlook.com>
2021-12-12 19:10:36 +0800

    build: update dependencies and rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -21,7 +21,7 @@ export default {
     exports: "default",
     banner,
   },
-  external: ["obsidian"],
+  external: ["obsidian", "@codemirror/state"],
   plugins: [
     typescript(),
     nodeResolve({ browser: true }),
