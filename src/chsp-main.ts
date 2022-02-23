4144b91cb7cbae824c3a3ec0193ac324b40851fa
aidenlx <aiden.lx@outlook.com>
2022-02-23 17:07:41 +0800

    revert: remove api

--- src/chsp-main.ts
+++ src/chsp-main.ts
@@ -1,25 +1,15 @@
 import { Plugin } from "obsidian";
-import pinyin from "pinyinlite";
 import { Segment, useDefault } from "segmentit";
 
-import { API_NAME, ChsPatchAPI, Evt_ApiReady } from "./api";
 import patchGetWordAt from "./cm5";
 import { getChsPatchExtension, getWordAtPatchUnloader } from "./cm6/index";
 
-const API_NAME: API_NAME extends keyof typeof window ? API_NAME : never =
-  "ChsPatchAPI" as const; // this line will throw error when name out of sync
-const chsRegex = /[\u4e00-\u9fa5]/g;
-
 export default class CMChsPatch extends Plugin {
-  api?: ChsPatchAPI;
-
   segmentit: any;
 
   async onload() {
     console.log("loading cm-chs-patch");
     this.segmentit = useDefault(new Segment());
-
-    this.loadApi();
 
     // for cm5
     const cm5PatchUnloader = patchGetWordAt(this.segmentit);
@@ -28,15 +18,5 @@ export default class CMChsPatch extends Plugin {
     // for cm6
     this.registerEditorExtension(getChsPatchExtension(this.segmentit));
     this.register(getWordAtPatchUnloader(this.segmentit));
-  }
-
-  loadApi() {
-    const api = { pinyin, chsRegex };
-    this.api = api;
-    window[API_NAME] = api;
-    this.register(() => (window[API_NAME] = undefined));
-    console.log("ChsPatch API ready");
-
-    this.app.metadataCache.trigger(...(<Evt_ApiReady>["chs-patch:ready", api]));
   }
 }
