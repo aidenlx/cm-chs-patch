94511a522cec38ee6e2d4bce0c2525da1584d3c3
jiyee <jiyee.sheng@gmail.com>
2023-01-02 22:42:52 +0800

    feat: support background download jieba-wasm file from GitHub

--- src/install-guide.ts
+++ src/install-guide.ts
@@ -1,5 +1,6 @@
 import { fileDialog } from "file-select-dialog";
-import { Modal } from "obsidian";
+import { Modal, Notice, requestUrl } from "obsidian";
+import { unzip } from 'unzipit';
 
 import type CMChsPatch from "./chsp-main";
 
@@ -9,18 +10,20 @@ const colorSuccess = "var(--background-modifier-success)",
 export default class GoToDownloadModal extends Modal {
   reloadButton: HTMLButtonElement | null = null;
   selectButton: HTMLButtonElement | null = null;
+  downloadButton: HTMLButtonElement | null = null;
 
   constructor(public plugin: CMChsPatch) {
     super(plugin.app);
     this.modalEl.addClass("zt-install-guide");
   }
 
-  downloadLink = `https://github.com/aidenlx/cm-chs-patch/blob/master/assets/jiaba-wasm/${this.libName}.zip?raw=true`;
+  downloadLink = `https://raw.githubusercontent.com/aidenlx/cm-chs-patch/main/assets/jiaba-wasm/${this.libName}.zip`;
   lanzoumLink = "https://wwe.lanzoum.com/igUPR00jp02h";
 
   get libName() {
     return this.plugin.libName;
   }
+
   onOpen() {
     this.contentEl.createEl("h1", { text: "安装结巴分词" });
     this.contentEl.createDiv({}, (div) => {
@@ -36,15 +39,23 @@ export default class GoToDownloadModal extends Modal {
           li.createEl("a", { href: this.downloadLink, text: "GitHub" });
           li.appendText(" 或 ");
           li.createEl("a", { href: this.lanzoumLink, text: "蓝奏云" });
-          li.appendText(" 下载");
+          li.appendText(" 手动下载");
+          li.appendText(" 或 ");
+
+          this.downloadButton = li.createEl(
+            "button",
+            { text: "自动下载" },
+            (btn) => (btn.onclick = this.onDownloadingFile.bind(this)),
+          );
         });
         ol.createEl("li", {}, (li) => {
-          li.appendText("解压缩zip包得到 ");
+          li.appendText("解压缩 zip 包得到 ");
           li.createEl("code", { text: this.libName });
         });
         ol.createEl("li", {}, (li) => {
           li.appendText("在弹出的窗口选择 ");
           li.createEl("code", { text: this.libName });
+          li.appendText("  ");
           this.selectButton = li.createEl(
             "button",
             { text: "选择文件" },
@@ -52,7 +63,7 @@ export default class GoToDownloadModal extends Modal {
           );
         });
         ol.createEl("li", {}, (li) => {
-          li.appendText("重新加载分词插件: ");
+          li.appendText("重新加载分词插件:  ");
           this.reloadButton = li.createEl(
             "button",
             { text: "重新加载" },
@@ -90,9 +101,39 @@ export default class GoToDownloadModal extends Modal {
       this.reloadButton.style.backgroundColor = "";
     }
   }
+  async onDownloadingFile() {
+    if (this.reloadButton) {
+      this.reloadButton.disabled = true;
+      this.reloadButton.style.backgroundColor = colorDisabled;
+    }
+
+    const resp = await requestUrl({url: this.downloadLink, contentType: "application/octet-stream"});
+    const { entries } = await unzip(resp.arrayBuffer);
+    if (entries[`${this.libName}`]) {
+      await this.app.vault.adapter.writeBinary(this.plugin.libPath, await entries[`${this.libName}`].arrayBuffer());
+    }
+
+    if (this.selectButton) {
+      this.selectButton.setText("结巴分词插件导入成功");
+      this.selectButton.style.backgroundColor = colorSuccess;
+    }
+    if (this.reloadButton) {
+      this.reloadButton.disabled = false;
+      this.reloadButton.style.backgroundColor = "";
+    }
+  }
   async onReloadPlugin() {
-    await this.app.plugins.disablePlugin(this.plugin.manifest.id);
-    this.close();
-    await this.app.plugins.enablePlugin(this.plugin.manifest.id);
+    if (await app.vault.adapter.exists(this.plugin.libPath, true)) {
+      const stat = await app.vault.adapter.stat(this.plugin.libPath);
+      if (stat && stat.type == "file" && stat.size > 0) {
+        await this.app.plugins.disablePlugin(this.plugin.manifest.id);
+        this.close();
+        await this.app.plugins.enablePlugin(this.plugin.manifest.id);
+        await this.app.setting.openTabById(this.plugin.manifest.id);
+      }
+      new Notice("✔️ 安装结巴分词插件成功");
+    } else {
+      new Notice("❌ 安装结巴分词插件失败");
+    }
   }
 }
