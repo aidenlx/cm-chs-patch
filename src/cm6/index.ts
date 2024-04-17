64618f01ac7b121e9ea157bc74aeb04e73f121dc
jiyee <jiyee.sheng@gmail.com>
2024-04-17 13:15:00 +0800

    FIX: support Option+Shift+Arrow select by chinese words #13 #27

--- src/cm6/index.ts
+++ src/cm6/index.ts
@@ -22,17 +22,76 @@ const setupCM6 = (plugin: CMChsPatch) => {
         },
     }),
   );
+
+  let origPos: number | null; // 记录 origPos, 判断光标转向
+
+  // 光标选择移动方向类型
+  enum Direction {
+    BeginAndForward     = "BeginAndForward",
+    BeginAndBackward    = "BeginAndBackward",
+    ForwardAndForward   = "ForwardAndForward",
+    ForwardAndBackward  = "ForwardAndBackward",
+    BackwardAndForward  = "BackwardAndForward",
+    BackwardAndBackward = "BackwardAndBackward",
+  }
+
   plugin.register(
     around(EditorView.prototype, {
       moveByGroup: (next) =>
         function (this: EditorView, start: SelectionRange, forward: boolean) {
           const dest = next.call(this, start, forward);
-          if (dest.empty && start.empty) {
+          if (dest.empty || start.empty) {
+	    let direction : Direction | null;
+            if (dest.empty && start.empty) {
+              direction = forward ? Direction.BeginAndForward : Direction.BeginAndBackward;
+              origPos = start.from;
+            } else {
+              if (forward) {
+                direction = origPos != start.to ? Direction.ForwardAndForward : Direction.BackwardAndForward;
+              } else {
+                direction = origPos != start.from ? Direction.BackwardAndBackward : Direction.ForwardAndBackward;
+              }
+            }
+
+            let startPos: number;
+            switch (direction) {
+              case Direction.BeginAndForward:
+                startPos = start.from;
+                break;
+              case Direction.BeginAndBackward:
+                startPos = start.to;
+                break;
+              case Direction.ForwardAndForward:
+                if (start.from <= dest.to) {
+                  startPos = start.to + 1;
+                } else {
+                  startPos = start.from + 1;
+                }
+                break;
+              case Direction.BackwardAndForward:
+                startPos = start.from + 1;
+                break;
+              case Direction.BackwardAndBackward:
+                if (start.from > dest.to) {
+                  startPos = start.from - 1;
+                } else {
+                  startPos = start.to;
+                }
+                break;
+              case Direction.ForwardAndBackward:
+                startPos = start.to;
+                break;
+              default:
+                startPos = start.from;
+                break;
+            }
+
             const destPos = plugin.getSegRangeFromGroup(
-              start.from,
-              dest.from,
+              startPos,
+              forward ? dest.from : dest.to,
               this.state.sliceDoc.bind(this.state),
-            );
+              );
+
             if (destPos) return EditorSelection.range(destPos, destPos);
           }
           return dest;
